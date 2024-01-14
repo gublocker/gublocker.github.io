4994d4e0608afa3dd7988eeda2f8cdb58d7a087f
gameub <elijahdeventer@gmail.com>
2024-01-14 13:25:12 -0500

    v4.0 - new UI

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ var splashes=[
     "Don't wake the sleeping pills!",
     "Sponsored by NordVPN!",
     "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    "The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.",
     "Minceraft.",
     "Don't click this.Because it does nothing.",
     "Check the changelog sometimes.",
