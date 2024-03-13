3a4dcba0e10d38dcd4c1df003766917f4a556746
artisticmclaren <elijahjvandeventer@gmail.com>
2024-03-12 21:01:57 -0400

    beta branch update

--- beta/script.js
+++ beta/script.js
@@ -1,6 +1,19 @@
 document.getElementById("betas").style.display="none";
 
+debug=false;
+
+if (debug) autoLogin();
+
 function CheckPassword() {
 	let pass = document.getElementById("pass");
 	if (pass.value=="hgcrq") { document.getElementById("betas").style.display="block"; pass.value=""; }
+}
+
+function autoLogin() {
+	document.getElementById("betas").style.display="block"; 
+	pass.value="DO NOT COMMIT";
+}
+
+function LoadURL(url) {
+	document.getElementById("loader").src = `https://${url}`;
 }
