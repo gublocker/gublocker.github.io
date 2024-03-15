f67c44664321506ce834d75dc95a4f65e763519a
artisticmclaren <elijahjvandeventer@gmail.com>
2024-03-15 19:17:39 -0400

    beta branch update

--- beta/script.js
+++ beta/script.js
@@ -4,9 +4,24 @@ debug=false;
 
 if (debug) autoLogin();
 
+let showpass = document.getElementById("show")
+let entered = document.getElementById("entered")
+
+entered.style.display="none";
+
+showpass.addEventListener("mouseenter", function (e) {
+	document.getElementById("pass").style.color="#000";
+},false);
+
+showpass.addEventListener("mouseleave", function (e) {
+	document.getElementById("pass").style.color="#fff";
+},false);
+
 function CheckPassword() {
 	let pass = document.getElementById("pass");
-	if (pass.value=="hgcrq") { document.getElementById("betas").style.display="block"; pass.value=""; }
+	if (pass.value=="hgcrq") { document.getElementById("betas").style.display="block"; document.getElementById("password-enter").style.display="none"; }
+	else { entered.style.display="block"; document.getElementById("entered-pass").innerHTML=pass.value; }
+	pass.value="";
 }
 
 function autoLogin() {
